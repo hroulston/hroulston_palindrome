c61db1d667097e65d54670587f259367eb92498c
hroulston <h_roulston@live.com>
2023-05-31 15:20:17 -0700

    Adds a test for literal palindrome and tests that are created but skipped so they are not forgotten.

--- test/test_hroulston_palindrome.rb
+++ test/test_hroulston_palindrome.rb
@@ -4,7 +4,19 @@ require "test_helper"
 
 class TestHroulstonPalindrome < Minitest::Test
  
-  def test_for_non_palindrome
+  def test_non_palindrome
     assert !"apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    skip 
+  end
+
+  def test_palindrome_with_punctuation
+    skip
   end
 end
