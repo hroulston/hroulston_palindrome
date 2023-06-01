53b8e4e7d670905b917140ea6c3881bcdd4de2e5
hroulston <h_roulston@live.com>
2023-06-01 11:38:24 -0700

    refactors letters method into oblivion and deletes letters test. All tests pass.

--- lib/hroulston_palindrome.rb
+++ lib/hroulston_palindrome.rb
@@ -8,21 +8,16 @@ class String
   end
 
   #Method should return the letters from a string
-  def letters
-    the_letters = []
-    #Round 1 for loop to get test to pass
-    for i in 0..self.length - 1 do
-      if self[i].match(/[a-zA-z]/)
-        the_letters << self[i]
-      end
-    end
-    #Now having the letters they are rejoined into a string
-    the_letters.join
-  end
+  #Completely refactored method
+  #Can actually delete method and go further creating 
+  #the pattern matching in processed content.
+  # def letters
+  #   chars.select { |c| c.match(/[a-z]/i) }.join
+  # end
 
   private
     
     def processed_content
-      self.letters.downcase
+      self.scan(/[a-z]/i).join.downcase
     end
 end
